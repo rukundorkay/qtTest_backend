d0f49e1d25893a9e07090a615d4f7bbbc2475c5d
RUKUNDO janvier <51247342+rukundorkay@users.noreply.github.com>
2023-11-28 12:19:24 +0200

    adding whitenoise to render

--- qtTest/settings.py
+++ qtTest/settings.py
@@ -149,6 +149,8 @@ REST_FRAMEWORK = {
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
